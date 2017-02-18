e5391e9e8989fa3ddfe9723c9efdc298bd4b3735
njdaniel <nicholasjdaniel@gmail.com>
2017-02-18 14:23:31 -0500

    change new_character to self

--- angrytoad/character.py
+++ angrytoad/character.py
@@ -22,20 +22,20 @@ class Character(object):
 
 # =================== Methods for Character ======================
 
-    def user_create_character(new_character):
+    def user_create_character(self):
         print("Please help finish")
         get_name = input("Please enter name: ")
-        new_character.set_name(get_name)
-        print("You have %d points to spend on Attributes:" % (new_character.points))
+        self.set_name(get_name)
+        print("You have %d points to spend on Attributes:" % (self.points))
 
         # Distributing the points
         while True:
             CHARACTER_POINTS_COMMANDS = ['add_points', 'remove_points', 'reset_points']
-            new_character._attributes = {'Strength': new_character.strength, 'Dexterity': new_character.dexterity,
-                                         'Constitution': new_character.constitution,
-                                         'Intelligence': new_character.intelligence, 'Wisdom': new_character.wisdom,
-                                         'Charisma': new_character.charisma}
-            new_character.print_attributes()
+            self._attributes = {'Strength': self.strength, 'Dexterity': self.dexterity,
+                                         'Constitution': self.constitution,
+                                         'Intelligence': self.intelligence, 'Wisdom': self.wisdom,
+                                         'Charisma': self.charisma}
+            self.print_attributes()
 
             command = input("Enter command to alter points"
                             "(ex. <add,remove>, <attribute>, <amount>"
@@ -46,20 +46,20 @@ class Character(object):
             if numbers_inlist:
                 amount = int(''.join(numbers_inlist))
             attribute_to_alter = ''
-            for key in new_character._attributes.items():
+            for key in self._attributes.items():
                 if key[0].lower() in command:
                     attribute_to_alter = key[0].lower()
             if 'clear' in command.lower():
-                new_character.reset_points()
-                new_character.print_attributes()
+                self.reset_points()
+                self.print_attributes()
             elif 'add' in command.lower():
-                new_character.add_points(amount, attribute_to_alter)
-                new_character.print_attributes()
+                self.add_points(amount, attribute_to_alter)
+                self.print_attributes()
             elif 'remove' in command.lower():
-                new_character.remove_points(amount, attribute_to_alter)
-                new_character.print_attributes()
+                self.remove_points(amount, attribute_to_alter)
+                self.print_attributes()
             elif 'confirm' in command.lower():
-                new_character.print_attributes()
+                self.print_attributes()
                 confirmation = input("Are you sure?[y/n] ")
                 if confirmation is 'y':
                     break
